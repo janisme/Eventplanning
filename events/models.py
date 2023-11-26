e453743a15d2ed8a6525ae71376451b624493415
Jan <janisme124@gmail.com>
2023-11-25 23:53:54 -0500

    V2.1 update migration setting and pwd
    Signed-off-by: Jan <janisme124@gmail.com>

--- events/models.py
+++ events/models.py
@@ -6,13 +6,13 @@ class Event(models.Model):
     event_title =models.CharField(max_length=25)
     holder_id = models.CharField(max_length=10)
     e_date = models.DateField()
-    duration = models.IntegerField()
-    e_time = models.TimeField()
+    #duration = models.IntegerField()
+    #e_time = models.TimeField()
     e_detail = models.TextField()
     e_complete = models.BooleanField(default=False)
     capacity = models.IntegerField(default=50)
-    place = models.CharField(max_length=10)
-    cat_indoor = models.BooleanField()
+    #place = models.CharField(max_length=10)
+    #cat_indoor = models.BooleanField()
     cat_category = models.CharField(
         max_length=10,
         choices=[('Career', 'Career'), ('Social', 'Social'), ('Sport', 'Sport'), ('Panel', 'Panel')],
